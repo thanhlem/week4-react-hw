74a73432e280e1db3bd7fffd71fa888a8a383fb2
thanhlem <thanhlm.rsc@gmail.com>
2023-01-09 15:56:26 +0700

    modify index and app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
 import React, { useState } from "react";
+import axios from "axios";
 
-import "./App.css";
+import "./index.css";
 
 export default function App() {
   const [city, setCity] = useState();
-  const [message, setMessage] = useState("");
+  //const [loaded, setLoaded] = useState(false);
+
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    //setLoaded(true);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: "http://openweathermap.org/img/wn/".concat(
+        response.data.weather[0].icon,
+        "@2x.png"
+      ),
+      description: response.data.weather[0].description,
+    });
+  }
+
   function handleSearch(event) {
     event.preventDefault();
-    if (city.length > 0) {
-      setMessage("It is currently 20°C in ".concat(city));
-    } else {
-      alert("Enter a city");
-    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7474903e58d91475b44694a60ba263f4&units=metric`;
+
+    axios.get(url).then(showWeather);
   }
   function updateCity(event) {
     setCity(event.target.value);
   }
+
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
+      <h1>Weather App</h1>
       <form onSubmit={handleSearch}>
         <input type="search" placeholder="Enter a city" onChange={updateCity} />
         <input type="submit" value="Search" />
       </form>
-      <h2>{message}</h2>
+      <ul>
+        <li>Temperature: {weather.temperature}°C</li>
+        <li>Description: {weather.description}</li>
+        <li>Humidity: {weather.humidity}%</li>
+        <li>Wind: {weather.wind}km/h</li>
+        <li>
+          <img src={weather.icon} alt={weather.description} />
+        </li>
+      </ul>
     </div>
   );
 }
